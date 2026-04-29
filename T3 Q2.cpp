378adfe0c5a14c0fa61c60a6bef4e6bf91cc50df
FARIS AMSYARI BIN SAIFUL FAZAMIL <you@example.com>
2026-04-29 13:52:08 +0800

    Tutorial 3 Q3

--- T3 Q2.cpp
+++ T3 Q2.cpp
@@ -6,8 +6,8 @@ void swap_v2(int &num1, int &num2);
 int main(){
 
     int x = 2, y = 4;
-    cout << "\n x =" << x;
-    cout << "\n y =" << y;
+    cout << "\n x = " << x;
+    cout << "\n y = " << y;
 
     swap(x, y);
     cout << "\n After swapping ";
